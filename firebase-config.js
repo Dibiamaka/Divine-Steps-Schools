83de35063c3e52cd1271ee7be1cc8b6c02861cfb
David Chinonso Nwonye <dibiamakajnrnwonye@gmail.com>
2024-04-05 07:44:59 -0700

    Create firebase-config.js
    firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAW7tayOKqL7g-fHLeQ9he_SMLLrFtzQB0",
+  authDomain: "divine-steps-chatroom.firebaseapp.com",
+  projectId: "divine-steps-chatroom",
+  storageBucket: "divine-steps-chatroom.appspot.com",
+  messagingSenderId: "542426854016",
+  appId: "1:542426854016:web:9ec3777db3fd2343266c65"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
